5dfab3afd14e6cf1ec0abf0788ddcf2e824c3250
Celso Brito <celso.botelho14@gmail.com>
2023-05-04 23:13:19 -0300

    feat: Construção da base da tela de login

---
+++ lib/utils/image_url.dart
@@ -0,0 +1,7 @@
+enum ImageUrl {
+  background('https://media.discordapp.net/attachments/1103835614467600394/1103845534038511726/fundo_do_app.png?width=569&height=569'),
+  logo('https://media.discordapp.net/attachments/1103835614467600394/1103845502178578452/Ipheira.jpg?width=569&height=569');
+
+  final String value;
+  const ImageUrl(this.value);
+}
