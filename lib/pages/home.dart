1b024c01bd86e3c78d5a63ef94e634a3e6c9adad
Celso Brito <celso.botelho14@gmail.com>
2023-05-05 20:13:07 -0300

    feat: colocando o scrollview no ipheira

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,103 +16,109 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: Center(
         child: Container(
+          height: 1000,
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(ImageUrl.background.value),
-              fit: BoxFit.scaleDown,
-              alignment: Alignment.bottomCenter
-            ),
+                  image: DecorationImage(
+                  image: NetworkImage(ImageUrl.background.value),
+                  fit: BoxFit.scaleDown,
+                  alignment: Alignment.bottomCenter // alterado aqui
+              ),
             color: Colors.white
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.white,
-                height: 200,
-                width: 225,
-                child: Image.network(ImageUrl.logo.value, fit: BoxFit.fill,),
-              ),
-              RichText(
-                textAlign: TextAlign.center,
-                text: const TextSpan(
-                  text: 'Bem-vindo\n',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: 'ao\n',
-                      style: TextStyle(color: Colors.black, fontSize: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  color: Colors.white,
+                  height: 200,
+                  width: 225,
+                  child: Image.network(ImageUrl.logo.value, fit: BoxFit.fill,),
+                ),
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    text: 'Bem-vindo\n',
+                    style: TextStyle(color: Colors.black, fontSize: 19),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'ao\n',
+                        style: TextStyle(color: Colors.black, fontSize: 19),
+                      ),
+                      TextSpan(
+                        text: 'Ipheira\n',
+                        style: TextStyle(color: Color.fromRGBO(77,167,104,1),
+                            fontSize: 19
+                      )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextFormField(
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Color.fromRGBO(200,200,200,1),
+                      filled: true,
                     ),
-                    TextSpan(
-                      text: 'Ipheira\n',
-                      style: TextStyle(color: Color.fromRGBO(77,167,104,1),
-                          fontSize: 16
-                    )),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextFormField(
-                  textAlign: TextAlign.start,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(color: Colors.black),
-                    fillColor: Color.fromRGBO(200,200,200,1),
-                    filled: true,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16),
-                child: TextFormField(
-                  textAlign: TextAlign.start,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Senha',
-                    hintStyle: TextStyle(color: Colors.black),
-                    fillColor: Color.fromRGBO(200,200,200,1),
-                    filled: true,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 8),
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(fontSize: 18),
-                      padding: const EdgeInsets.symmetric(horizontal: 33, vertical: 13)
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextFormField(
+                    textAlign: TextAlign.start,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Senha',
+                      hintStyle: TextStyle(color: Colors.black),
+                      fillColor: Color.fromRGBO(200,200,200,1),
+                      filled: true,
                     ),
-                    child: const Text("Login"),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  // ação a ser executada ao clicar no hyperlink
-                },
-                child: const Text(
-                  'Ainda não tem conta? cadastre-se agora',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                    color: Color.fromRGBO(0,102,51,1)
                   ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  // ação a ser executada ao clicar no hyperlink
-                },
-                child: const Text(
-                  'Esqueceu a senha?',
-                  style: TextStyle(
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 8),
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 18),
+                        padding: const EdgeInsets.symmetric(horizontal: 33, vertical: 13)
+                      ),
+                      child: const Text("Login"),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    // ação a ser executada ao clicar no hyperlink
+                  },
+                  child: const Text(
+                    'Ainda não tem conta? cadastre-se agora',
+                    style: TextStyle(
                       decoration: TextDecoration.underline,
-                      color: Colors.red
+                      color: Color.fromRGBO(0,102,51,1)
+                    ),
                   ),
                 ),
-              ),
-            ],
+                GestureDetector(
+                  onTap: () {
+                    // ação a ser executada ao clicar no hyperlink
+                  },
+                  child: const Text(
+                    'Esqueceu a senha?',
+                    style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        color: Colors.red
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
